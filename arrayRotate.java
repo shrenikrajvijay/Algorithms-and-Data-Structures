549e4a31715c3a0c8a6220ae79346b12c301b6c8
V <s.vijay0991@yahoo.com>
2017-12-05 21:08:21 -0800

    added more file

--- arrayRotate.java
+++ arrayRotate.java
@@ -1,3 +1,6 @@
+
+import java.util.Arrays;
+
 /* @author vijayshrenikraj
  Write a function rotate(ar[], d, n) that rotates arr[] of size n by d elements.
 i/p {1, 2, 3, 4, 5, 6, 7}, i=2
@@ -6,8 +9,26 @@ o/p 3 4 5 6 7 1 2
 
 public class arrayRotate {
     public static void main(String[] args) {
-        System.out.println("Using Hashset, O(N) :");
-        //usingHashset(A, n);
+        int [] arr = {1, 2, 3, 4, 5, 6, 7};
+        int d=2;
+        System.out.println("Moving temporary array, O(N) :");
+        usingTemporaryArray(arr.clone(), d);
+    }
+
+    private static void usingTemporaryArray(int[] arr, int d) {
+        int [] temp = new int[d];
+        int i;
+        for(i=0;i<d;i++){
+            temp[i] = arr[i];
+        }
+        for(i=i;i<arr.length;i++){
+            arr[i-d] = arr[i];
+        }
+        for(int j=0;j<d;j++){
+            arr[i-d] = temp[j];
+            i++;
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
 
